4a288c2d0e3feb9206b1ad13ba381c11c20798e4
ajayparmar311 <ajayp.parmar311@gmail.com>
2025-08-05 14:43:39 +0530

    add new schema

--- pubsub.tf
+++ pubsub.tf
@@ -19,39 +19,8 @@ resource "google_bigquery_table" "metrics_table" {
   dataset_id = google_bigquery_dataset.otel_metrics.dataset_id
   table_id   = "metrics_table"
   project = var.project_id
+  schema     = file("${path.module}/schemas/metrics_table_schema.json")
 
-  schema = jsonencode([
-    {
-      name = "metric_name",
-      type = "STRING",
-      mode = "REQUIRED"
-    },
-    {
-      name = "metric_value",
-      type = "FLOAT",
-      mode = "REQUIRED"
-    },
-    {
-      name = "timestamp",
-      type = "TIMESTAMP",
-      mode = "NULLABLE"
-    },
-    {
-      name = "labels",
-      type = "RECORD",
-      mode = "NULLABLE",
-      fields = [
-        { name = "app_name",     type = "STRING", mode = "NULLABLE" },
-        { name = "cam_id",       type = "STRING", mode = "NULLABLE" },
-        { name = "error_type",   type = "STRING", mode = "NULLABLE" },
-        { name = "filter_type",  type = "STRING", mode = "NULLABLE" },
-        { name = "store",        type = "STRING", mode = "NULLABLE" },
-        { name = "type",         type = "STRING", mode = "NULLABLE" },
-        { name = "region",       type = "STRING", mode = "NULLABLE" },   # for future
-        { name = "env",          type = "STRING", mode = "NULLABLE" }    # for future
-      ]
-    }
-  ])
 }
 
 
@@ -64,7 +33,7 @@ resource "google_pubsub_subscription" "bigquery_sub" {
   bigquery_config {
     table = "${google_bigquery_table.metrics_table.project}.${google_bigquery_dataset.otel_metrics.dataset_id}.${google_bigquery_table.metrics_table.table_id}"
     use_topic_schema = false
-    write_metadata   = false
+    write_metadata   = true
   }
 
   ack_deadline_seconds = 20
