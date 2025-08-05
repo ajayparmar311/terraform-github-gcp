9e3f75a789aa15be82e7a8349ec09426f1fbf476
ajayparmar311 <ajayp.parmar311@gmail.com>
2025-08-05 15:11:49 +0530

    adding new table

--- pubsub.tf
+++ pubsub.tf
@@ -20,8 +20,46 @@ resource "google_bigquery_table" "metrics_table" {
   table_id   = "metrics_table"
   deletion_protection = false
   project = var.project_id
-  schema     = file("${path.module}/schemas/metrics_table_schema.json")
+  schema = jsonencode([
+    {
+      name = "metric_name",
+      type = "STRING",
+      mode = "REQUIRED"
+    },
+    {
+      name = "metric_value",
+      type = "FLOAT",
+      mode = "REQUIRED"
+    },
+    {
+      name = "timestamp",
+      type = "TIMESTAMP",
+      mode = "NULLABLE"
+    },
+    {
+      name = "labels",
+      type = "RECORD",
+      mode = "NULLABLE",
+      fields = [
+        { name = "app_name",     type = "STRING", mode = "NULLABLE" },
+        { name = "cam_id",       type = "STRING", mode = "NULLABLE" },
+        { name = "error_type",   type = "STRING", mode = "NULLABLE" },
+        { name = "filter_type",  type = "STRING", mode = "NULLABLE" },
+        { name = "store",        type = "STRING", mode = "NULLABLE" },
+        { name = "type",         type = "STRING", mode = "NULLABLE" },
+        { name = "region",       type = "STRING", mode = "NULLABLE" },   # for future
+        { name = "env",          type = "STRING", mode = "NULLABLE" }    # for future
+      ]
+    }
+  ])
+}
 
+resource "google_bigquery_table" "metrics_table_otel" {
+  dataset_id = google_bigquery_dataset.otel_metrics.dataset_id
+  table_id   = "metrics_table_otel"
+  deletion_protection = false
+  project = var.project_id
+  schema     = file("${path.module}/schemas/metrics_table_schema.json")
 }
 
 
@@ -32,7 +70,7 @@ resource "google_pubsub_subscription" "bigquery_sub" {
   topic = google_pubsub_topic.otel_metrics.id
 
   bigquery_config {
-    table = "${google_bigquery_table.metrics_table.project}.${google_bigquery_dataset.otel_metrics.dataset_id}.${google_bigquery_table.metrics_table.table_id}"
+    table = "${google_bigquery_table.metrics_table_otel.project}.${google_bigquery_dataset.otel_metrics.dataset_id}.${google_bigquery_table.metrics_table_otel.table_id}"
     use_topic_schema = false
     write_metadata   = true
   }
