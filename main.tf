258d508a4c86edbd66b55d9f234f48e31bc3b33b
ajayparmar311 <ajayp.parmar311@gmail.com>
2025-09-02 23:24:06 +0530

    adding container repo

--- main.tf
+++ main.tf
@@ -1,10 +1,17 @@
 terraform {
+  required_version = ">= 1.0"
+
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 4.0"
+      version = "~> 5.0"
+    }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "~> 5.0"
     }
   }
+}
 
   # Optional: Remote state in GCS (recommended)
   backend "gcs" {
